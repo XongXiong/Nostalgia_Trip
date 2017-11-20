5b61d71c1bdf4f81fda0c2cfacacd1c113ea48c8
Xong Xiong <xxiong3153@gmail.com>
2017-11-19 20:14:32 -0600

    templated finished

--- server/public/scripts/controllers/post.controller.js
+++ server/public/scripts/controllers/post.controller.js
@@ -3,6 +3,12 @@ myApp.controller('PostController', function ($http, UserService) {
     let vm = this;
     
     vm.posts = [{ name: 'Shoua', url: 'https://scontent.ffar1-1.fna.fbcdn.net/v/t31.0-8/22555659_1727158440629943_40649371309642927_o.jpg?oh=1fad174b1e41ed40d5ec9f5d6eb8d3b6&oe=5A617EB0', desc: 'The most', ref: 'http://lmgtfy.com/?q=Prime+Digital+Academy', votes: 1000000 }, { name: 'Shoua', url: 'https://scontent.ffar1-1.fna.fbcdn.net/v/t31.0-8/22555659_1727158440629943_40649371309642927_o.jpg?oh=1fad174b1e41ed40d5ec9f5d6eb8d3b6&oe=5A617EB0', desc: 'The most', ref: 'http://lmgtfy.com/?q=Prime+Digital+Academy', votes: 1000000 }, { name: 'Shoua', url: 'https://scontent.ffar1-1.fna.fbcdn.net/v/t31.0-8/22555659_1727158440629943_40649371309642927_o.jpg?oh=1fad174b1e41ed40d5ec9f5d6eb8d3b6&oe=5A617EB0', desc: 'The most', ref: 'http://lmgtfy.com/?q=Prime+Digital+Academy', votes: 1000000 }, { name: 'Shoua', url: 'https://scontent.ffar1-1.fna.fbcdn.net/v/t31.0-8/22555659_1727158440629943_40649371309642927_o.jpg?oh=1fad174b1e41ed40d5ec9f5d6eb8d3b6&oe=5A617EB0', desc: 'The most', ref: 'http://lmgtfy.com/?q=Prime+Digital+Academy', votes: 1000000 }, ];
+    let postToAdd = {
+        postName: '',
+        postDesc: '',
+        postPic: ''
+    }
+
 
     UserService.getuser();
 
@@ -13,7 +19,13 @@ myApp.controller('PostController', function ($http, UserService) {
         $http.post('/post', post).then(function(response) {
             console.log(response);
             let lmgtfyUrl = response.data;
-            window.location.href = lmgtfyUrl;
+            window.open(lmgtfyUrl, "_blank"); 
+        })
+    }
+
+    vm.addPost = () => {
+        $http.post('/post/add', postToAdd).then(function(response) {
+            console.log(response);
         })
     }
 });
--- server/routes/post.router.js
+++ server/routes/post.router.js
@@ -3,11 +3,16 @@ var router = express.Router();
 const lmgtfy = require('lmgtfy');
 
 router.post('/', function (req, res) {
-    console.log('get /post route');
+    console.log('post /post route');
     console.log(req.body);
     let postName = req.body.name;
     let searchResult = lmgtfy(postName);
     res.send(searchResult);
 });
+
+router.post('/add', function(req, res) {
+    console.log('post /post/add route');
+    console.log(req.body);
+})
 
 module.exports = router;
