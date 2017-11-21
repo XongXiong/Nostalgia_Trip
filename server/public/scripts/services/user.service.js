50067f6377340569d47e4055bb8e5cfe5b634033
Xong Xiong <xxiong3153@gmail.com>
2017-11-20 19:20:53 -0600

    working on authentication

--- server/public/scripts/services/user.service.js
+++ server/public/scripts/services/user.service.js
@@ -5,15 +5,26 @@ myApp.service('UserService', function($http, $location){
   self.loggedIn = false;
   self.posts = {data: []};
 
+  self.getAllPosts = () => {
+    $http.get('/post').then(function (response) {
+      console.log(response.data);
+      self.posts.data = response.data;
+    }).catch(function (err) {
+      console.log('Could not get all posts');
+    });
+  }
+
   self.getuser = () => {
     console.log('UserService -- getuser');
     $http.get('/user').then(function(response) {
         if(response.data.username) {
             // user has a curret session on the server
+            console.log(response);
             self.userObject.userName = response.data.username;
             self.userObject.id = response.data.id;
             console.log('UserService -- getuser -- User Data: ', self.userObject.userName);
         } else {
+          console.log(response);
             console.log('UserService -- getuser -- failure');
             // user has no session, bounce them back to the login page
             $location.path("/home");
@@ -34,15 +45,6 @@ myApp.service('UserService', function($http, $location){
 
   self.toggleLog = () => {
     self.loggedIn = !self.loggedIn;
-  }
-
-  self.getAllPosts = () => {
-    $http.get('/post').then(function (response) {
-      console.log(response.data);
-      self.posts.data = response.data;
-    }).catch(function (err) {
-      console.log('Could not get all posts');
-    });
   }
 
   self.postVote = (upDown, postId, votes) => {
